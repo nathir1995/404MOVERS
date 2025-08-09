073d2d9f9a322d651e3a0069fc15bae62cf5b80f
nathir1995 <101818747+nathir1995@users.noreply.github.com>
2025-08-09 09:36:13 -0600

    Update _app.tsx

--- mover/src/pages/_app.tsx
+++ mover/src/pages/_app.tsx
@@ -9,6 +9,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { NextPageWithLayout } from "@/layout/types";
 import type { AppProps } from "next/app";
 
+// NEW: import the ErrorBoundary component (place ErrorBoundary.tsx in your components folder)
+import ErrorBoundary from "@/components/ErrorBoundary";
+
 import { FirebaseContextProvider } from "@/firebase/FirebaseContext";
 
 type AppPropsWithLayout = AppProps & {
@@ -19,15 +22,16 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <FirebaseContextProvider>
-      <AuthProvider>
-        <QueryClientProvider client={queryClient}>
-          <AppHead />
-
-          {getLayout(<Component {...pageProps} />)}
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        </QueryClientProvider>
-      </AuthProvider>
-    </FirebaseContextProvider>
+    <ErrorBoundary>
+      <FirebaseContextProvider>
+        <AuthProvider>
+          <QueryClientProvider client={queryClient}>
+            <AppHead />
+            {getLayout(<Component {...pageProps} />)}
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+          </QueryClientProvider>
+        </AuthProvider>
+      </FirebaseContextProvider>
+    </ErrorBoundary>
   );
 }
