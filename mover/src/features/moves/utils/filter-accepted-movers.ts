6770bb311e0dda302b882826bb77cba0ae89b684
nathir1995 <101818747+nathir1995@users.noreply.github.com>
2025-08-09 00:28:02 -0600

    Update filter-accepted-movers.ts

--- mover/src/features/moves/utils/filter-accepted-movers.ts
+++ mover/src/features/moves/utils/filter-accepted-movers.ts
@@ -2,6 +2,14 @@ import { ROLE } from "@/constants/roles";
 import { User } from "@/features/auth/utils/AuthContextType";
 
 export const filterAcceptedMovers = (
-  movers: User[],
+  movers: User[] | undefined | null,
   moverType: ROLE.DRIVER | ROLE.LABOR
-): User[] => movers.filter((mover) => mover.user_role.key === moverType);
+): User[] => {
+  if (!movers || !Array.isArray(movers)) {
+    return [];
+  }
+  
+  return movers.filter((mover) => {
+    return mover?.user_role?.key === moverType;
+  });
+};
