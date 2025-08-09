883e62b634772e489d4a3d7ae5d7fbb993c250d3
nathir1995 <101818747+nathir1995@users.noreply.github.com>
2025-08-09 14:10:29 -0600

    Update safeArray.ts

--- mover/src/utility/safeArray.ts
+++ mover/src/utility/safeArray.ts
@@ -1,6 +1,7 @@
-export function a<T>(v: T[] | undefined | null): T[] {
+export function a<T>(v: T[] | null | undefined): T[] {
   return Array.isArray(v) ? v : [];
 }
-export function findSafe<T>(v: T[] | undefined | null, cb: (x: T) => boolean): T | undefined {
+
+export function findSafe<T>(v: T[] | null | undefined, cb: (x: T) => boolean): T | undefined {
   return a(v).find(cb);
 }
